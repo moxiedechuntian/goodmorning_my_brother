f81f138e6adf4e446e5c3cba0b74515b68463ed9
moxiedechuntian <74692376+moxiedechuntian@users.noreply.github.com>
2022-08-24 03:25:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
-user_id1 = "oSXgy53aGYcEwYTTKTterX8GvwXo"
 
 template_id = os.environ["TEMPLATE_ID"]
 
@@ -82,6 +81,6 @@ data = {"weather":{"value":wea},"temperature":{"value":temperature},
         "birthday_left6":{"value":birthday6},
         "words":{"value":get_words(), "color":get_random_color()}
         }
-res = wm.send_template(user_id,user_id1, template_id, data)
+res = wm.send_template(user_id, template_id, data)
 print(res)
 
