86af4d71b6284c9400713df18b4fddacab353cc4
moxiedechuntian <74692376+moxiedechuntian@users.noreply.github.com>
2022-08-24 03:00:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 
 city = os.environ['CITY']
-#city1 = os.environ['CITY1']
 city1 = "西安"
+city2 = "北京"
 birthday = os.environ['BIRTHDAY']
+birthday1 = os.environ['BIRTHDAY1']
+birthday2 = os.environ['BIRTHDAY2']
+birthday3 = os.environ['BIRTHDAY3']
+birthday4 = os.environ['BIRTHDAY4']
+birthday5 = os.environ['BIRTHDAY5']
+birthday6 = os.environ['BIRTHDAY6']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
@@ -28,16 +34,26 @@ def get_weather(a):
 
 wea, temperature= get_weather(city)
 wea1, temperature1= get_weather(city1)
+wea2, temperature2= get_weather(city1)
+
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
+def get_birthday(d):
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
+
+birthday = get_birthday(birthday)
+birthday1 = get_birthday(birthday1)
+birthday2 = get_birthday(birthday2)
+birthday3 = get_birthday(birthday3)
+birthday4 = get_birthday(birthday4)
+birthday5 = get_birthday(birthday5)
+birthday6 = get_birthday(birthday6)
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -53,8 +69,15 @@ wm = WeChatMessage(client)
 
 data = {"weather":{"value":wea},"temperature":{"value":temperature},
         "weather1":{"value":wea1},"temperature1":{"value":temperature1},
+        "weather2":{"value":wea2},"temperature2":{"value":temperature2},
         "love_days":{"value":get_count()},
-        "birthday_left":{"value":get_birthday()},
+        "birthday_left":{"value":birthday},
+        "birthday_left1":{"value":birthday1},
+        "birthday_left2":{"value":birthday2},
+        "birthday_left3":{"value":birthday3},
+        "birthday_left4":{"value":birthday4},
+        "birthday_left5":{"value":birthday5},
+        "birthday_left6":{"value":birthday6},
         "words":{"value":get_words(), "color":get_random_color()}
         }
 res = wm.send_template(user_id, template_id, data)
