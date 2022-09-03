8644b07f49a5d25ab338fc0b138b3ddbd744ee66
moxiedechuntian <74692376+moxiedechuntian@users.noreply.github.com>
2022-09-03 23:00:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ birthday5 = get_birthday(birthday5)
 birthday6 = get_birthday(birthday6)
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/pyq")
+  words = requests.get("https://api.shadiao.pro/du")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
